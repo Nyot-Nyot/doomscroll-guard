de10332e1429980d41fe862527b54fc801db58f9
Nyot-Nyot <dzakisultan012@gmail.com>
2026-05-14 16:05:39 +0700

    Bootstrap Flutter project scaffold

---
+++ lib/core/services/native_bridge.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/services.dart';
+
+class NativeBridge {
+  static const MethodChannel _channel = MethodChannel('doomscroll_guard/channel');
+
+  Future<bool> startService() async {
+    final result = await _channel.invokeMethod<bool>('startService');
+    return result ?? false;
+  }
+
+  Future<bool> stopService() async {
+    final result = await _channel.invokeMethod<bool>('stopService');
+    return result ?? false;
+  }
+
+  Future<Map<String, dynamic>> getMonitoringState() async {
+    final result = await _channel.invokeMethod<Map>('getMonitoringState');
+    return Map<String, dynamic>.from(result ?? {});
+  }
+
+  Future<List<dynamic>> getUsageStats() async {
+    final result = await _channel.invokeMethod<List>('getUsageStats');
+    return result ?? [];
+  }
+}
